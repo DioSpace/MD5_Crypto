e1b84da0101350c257349effeb6c9ecf4b1ec8b4
Dio <zhendong2011@live.cn>
2020-11-23 02:17:03 +0800

    delete
    删除无用代码

--- src/my/com/Main.java
+++ src/my/com/Main.java
@@ -1,13 +1,16 @@
 package my.com;
 
+import java.util.LinkedHashMap;
+
 public class Main {
 
-    private static String plaintext = "this is my test";
+    private static String plaintext = "com.youxiang.soyoungapp1591965539332";
     private static String ciphertext = "";
 
     public static void main(String[] args) {
         //c8aecc6dc33b9651e81b20b22806d7d1
         ciphertext = MD5.getMD5(plaintext, 32);
         System.out.println("result:\n" + ciphertext);
+        
     }
 }

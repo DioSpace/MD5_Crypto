b8dabbd0c3c406abf79f8a67344d72ea48e59776
Jobs8w <job8w@outlook.com>
2021-05-26 04:28:04 +0800

    some little

--- src/com/my/Main.java
+++ src/com/my/Main.java
@@ -2,7 +2,7 @@ package com.my;
 
 public class Main {
 
-    private static String plaintext = "35354759457138217852516642742746639193200";
+    private static String plaintext = "thisisatest";
     private static String ciphertext = "";
 
     public static void main(String[] args) {
